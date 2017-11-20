7636554938ea782ccc992301771fcea28938475c
Pavithra Rajan <pr384@njit.edu>
2017-11-19 23:25:00 -0500

    formatting added

--- index.php
+++ index.php
@@ -20,13 +20,15 @@ class Manage {
 spl_autoload_register(array('Manage', 'autoload'));
 
 $findall_accounts = accounts::findAll();
-$findone_account = accounts::findOne(1);
+$findone_account = accounts::findOne(13);
 $findall_todos = todos::findAll();
 $findone_todo = todos::findOne(1);
 ?>
+
+<h1>Select all records : Accounts</h1>
 <table border="0">
-    <tr><th>Select all Account records</th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             accounts::printHeaders($findall_accounts[0]);
         ?>
@@ -36,9 +38,14 @@ $findone_todo = todos::findOne(1);
     accounts::printAll($findall_accounts);
        
 ?>
+
+<hr>
+
+<h1>Select One Record : Accounts </h1>
+<p> Selected id: 1 </p>
 <table border="0">
-    <tr><th>Select One Account record </th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             accounts::printHeaders($findone_account);
         ?>
@@ -47,20 +54,27 @@ $findone_todo = todos::findOne(1);
 <?php 
     accounts::printOne($findone_account); 
 ?>
+<hr>
+
+<h1>Select all records : Todos</h1>
 <table border="0">
-    <tr><th>Select all Todos records</th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
-            todos::printHeaders($all_todos[0]);
+            todos::printHeaders($findall_todos[0]);
         ?>
     </tr>
 
 <?php 
-    todos::printAll($all_todos);
+    todos::printAll($findall_todos);
 ?>
+<hr>
+
+<h1>Select one records : Todos</h1>
+<p> Selected id: 1 </p>
 <table border="0">
     <tr><th>Select one todo record</th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             todos::printHeaders($findone_todo);
         ?>
@@ -72,19 +86,23 @@ $findone_todo = todos::findOne(1);
 
 $new_account = new account();
 $new_account->id = '';
-$new_account->email = 'test1@gmail.com';
-$new_account->fname = 'Test';
-$new_account->lname = 'User1';
-$new_account->phone = '87612367890';
-$new_account->birthday= '1993-03-06';
-$new_account->gender = 'female';
-$new_account->password = '1234';
+$new_account->email = 'wer345@gmail.com';
+$new_account->fname = 'Welcome';
+$new_account->lname = 'WSD';
+$new_account->phone = '12345678';
+$new_account->birthday= '2000-11-12';
+$new_account->gender = 'male';
+$new_account->password = '345';
 $new_account->save();
 $findall_accounts = accounts::findAll();
 ?>
+<hr>
+<h1>Select all records with newly added record: Accounts</h1>
+<p> Please find newly added record at the last</p>
+
 <table border="0">
-    <tr><th>New Insterted record is at the bottom</th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             accounts::printHeaders($findall_accounts[0]);
         ?>
@@ -92,21 +110,28 @@ $findall_accounts = accounts::findAll();
 
 <?php 
     accounts::printAll($findall_accounts);
+    ?>
 
+ <hr>
+
+<?php
 $record = new todo();
 $record->id = '';
 $record->owneremail = $new_account->email;
 $record->ownerid = $new_account->id;
-$record->createddate = '2017-11-13';
-$record->duedate = '2017-11-16';
-$record->message = 'Updating todos';
+$record->createddate = '2016-4-3';
+$record->duedate = '2018-3-3';
+$record->message = 'todos added';
 $record->isdone = 0;
 $record->save();
 $all_todos = todos::findAll();
 ?>
+
+<h1>Select all records with newly added record: Todos</h1>
+<p> Please find newly added record at the last</p>
 <table border="0">
-    <tr><th>Newly Insterted record is at the bottom</th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             todos::printHeaders($findall_todos[0]);
         ?>
@@ -114,51 +139,60 @@ $all_todos = todos::findAll();
 
 <?php 
     todos::printAll($findall_todos);
+?>
 
-$new_account = accounts::findOne(9);
+<hr>
+<?php
+$new_account = accounts::findOne(13);
 if($new_account != null)
 {    
 ?>
+
+<h1>Before Accounts Update </h1>
 <table border="0">
-    <tr><th>Before Accounts Update </th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             accounts::printHeaders($new_account);
         ?>
     </tr>
+    <hr>
 
 <?php 
     accounts::printOne($new_account);
 
-$new_account->email='newUpdatedEmail@gmail.com';
+$new_account->email='new@gmail.com';
 $new_account->save();
 
 ?>
+<h1>After Accounts Update </h1>
 <table border="0">
-    <tr><th>After Accounts Update </th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             accounts::printHeaders($new_account);
         ?>
     </tr>
-
+<hr>
 <?php 
     accounts::printOne($new_account);
 }
 else 
-    echo "<h3>No record found to update the account, for the ID passed.</h3>";
+    echo "<h3>No record found</h3>";
 
 $record=todos::findOne(4);
 if($record != null){
 ?>
+
+<h1>Before Todos Update </h1>
 <table border="0">
-    <tr><th>Before Todos Update </th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             todos::printHeaders($record);
         ?>
     </tr>
-
+<hr>
 <?php 
     todos::printOne($record);
 
@@ -166,28 +200,32 @@ $record->isdone=1;
 $record->save();
 
 ?>
+<h1>After Todos Update </h1>
 <table border="0">
-    <tr><th>After Todos Update </th></tr>
-    <tr COLSPAN=2 BGCOLOR="#55ff00">
+<table border="0">
+    
+    <tr COLSPAN=2 BGCOLOR="#f2dacd">
         <?php
             todos::printHeaders($record);
         ?>
     </tr>
-
+<hr>
 <?php 
     todos::printOne($record);
 }
 else 
-    echo "<h3>No record found to update the todo, for the ID passed.</h3>";
+    echo "<h3>No record found </h3>";
 
 $new_account = accounts::findOne(31);
 if($new_account != null){
     $new_account->delete();
     $findall_accounts = accounts::findAll();
     ?>
+    <h1>Before Accounts Delete </h1>
+	<table border="0">
     <table border="0">
-        <tr><th>Before deleting in Accounts</th></tr>
-        <tr COLSPAN=2 BGCOLOR="#55ff00">
+        
+        <tr COLSPAN=2 BGCOLOR="#f2dacd">
             <?php
                 accounts::printHeaders($findall_accounts[0]);
             ?>
@@ -198,9 +236,11 @@ if($new_account != null){
 
     $findall_accounts = accounts::findAll();
     ?>
+<hr>
+    <h1>After Accounts Delete </h1>
     <table border="0">
-        <tr><th>After deleting in Accounts</th></tr>
-        <tr COLSPAN=2 BGCOLOR="#55ff00">
+        
+        <tr COLSPAN=2 BGCOLOR="#f2dacd">
             <?php
                 accounts::printHeaders($findall_accounts[0]);
             ?>
@@ -210,16 +250,18 @@ if($new_account != null){
         accounts::printAll($findall_accounts);
 }
 else
-    echo "<h1>No record found or already the account has been deleted</h1>";
+    echo "<h1>No record found. Check if the record has been deleted already</h1>";
 
 $record = todos::findOne(10);
 if($record != null){
     $record->delete();
     $all_todos = todos::findAll();
     ?>
+    <hr>
+    <h1>Before Todos Delete </h1>
     <table border="0">
-        <tr><th>Before deleting in Todos</th></tr>
-        <tr COLSPAN=2 BGCOLOR="#55ff00">
+        
+        <tr COLSPAN=2 BGCOLOR="#f2dacd">
             <?php
                 todos::printHeaders($findall_todos[0]);
             ?>
@@ -229,9 +271,11 @@ if($record != null){
         todos::printAll($findall_todos);
         $all_todos = todos::findAll();
     ?>
+    <hr>
+    <h1>After Todos Delete </h1>
     <table border="0">
         <tr><th>After deleting in Todos</th></tr>
-        <tr COLSPAN=2 BGCOLOR="#55ff00">
+        <tr COLSPAN=2 BGCOLOR="#f2dacd">
             <?php
                 todos::printHeaders($findall_todos[0]);
             ?>
@@ -242,5 +286,5 @@ if($record != null){
 }
 
 else
-    echo "<h1>No record found or already the todo has been deleted</h1>";
+    echo "<h1>No record found. Check if the record has been deleted already</h1>";
 ?>
